fd1ba85e5e26def05a8db2f5aec6ea4e737033e8
khosro <khosro@roy>
2015-02-01 15:16:39 -0500

    addd new Drivers class

---
+++ testApp/src/testApp/Drivers.java
@@ -0,0 +1,13 @@
+package testApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Drivers {
+	List<Driver> drivers;
+	
+	public Drivers(){
+		
+		drivers = new ArrayList<Driver>();
+	}
+}
